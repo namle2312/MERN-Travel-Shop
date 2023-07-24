157f0c0bb5f5e36e031624961de352d98866dc1d
Le Ngoc Nam <yo231269@gmail.com>
2023-07-24 15:59:30 +0700

    Back end

---
+++ backend/routes/tours.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import {
+    createTour,
+    deleteTour,
+    getAllTour,
+    getSingleTour,
+    updateTour
+} from '../controllers/tourController.js'
+
+const router = express.Router()
+
+// create new tour
+router.post('/', createTour);
+
+// update new tour
+router.put('/:id', updateTour);
+
+// delete new tour
+router.delete('/:id', deleteTour);
+
+// get single tour
+router.get('/:id', getSingleTour);
+
+// get all tour
+router.get('/', getAllTour);
+
+export default router;
