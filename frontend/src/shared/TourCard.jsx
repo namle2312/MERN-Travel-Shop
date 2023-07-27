5ce613bbf87231f21cc37aaff34e6e064008eae7
Le Ngoc Nam <yo231269@gmail.com>
2023-07-27 18:34:29 +0700

    Tour-Card (some bug need fix)

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -7,7 +7,7 @@ import './tour-card.css'
 
 const TourCard = ({ tour }) => {
 
-    const { id, title, city, photo, price, featured, reviews } = tour;
+    const { _id, title, city, photo, price, featured, reviews } = tour;
 
     const { totalRating, avgRating } = calculateAvgRating(reviews)
 
@@ -36,7 +36,7 @@ const TourCard = ({ tour }) => {
                     </span>
                 </div>
 
-                <h5 className="tour__title"><Link to={`/tours/${id}`}  >{title}</Link></h5>
+                <h5 className="tour__title"><Link to={`/tours/${_id}`}  >{title}</Link></h5>
 
                 <div className="card__bottom d-flex align-items-center justify-content-between mt-3">
 
@@ -44,7 +44,7 @@ const TourCard = ({ tour }) => {
                         ${price} <span> /per person/</span>
                     </h5>
                     <button className='btn booking__btn'>
-                        <Link to={`/tours/${id}`}>Book Now</Link>
+                        <Link to={`/tours/${_id}`}>Book Now</Link>
                     </button>
                 </div>
             </CardBody>
