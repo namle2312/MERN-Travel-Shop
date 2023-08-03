6c84653d729d99b02495b7c6fab3599c20ffa33f
Le Ngoc Nam <yo231269@gmail.com>
2023-08-04 06:57:26 +0700

    menu reponsive

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -26,6 +26,7 @@ const nav__links = [
 const Header = () => {
 
     const headerRef = useRef(null);
+    const menuRef = useRef(null);
     const navigate = useNavigate()
     const { user, dispatch } = useContext(AuthContext)
 
@@ -47,7 +48,9 @@ const Header = () => {
     useEffect(() => {
         stickyHeaderFunc()
         return window.removeEventListener('scroll', stickyHeaderFunc)
-    })
+    });
+
+    const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
 
     return <header className='header' ref={headerRef}>
         <Container>
@@ -59,7 +62,7 @@ const Header = () => {
                     </div>
                     {/* ============ logo end ========= */}
                     {/* ============ menu start ========= */}
-                    <div className="navigation">
+                    <div className="navigation" ref={menuRef} onClick={toggleMenu}>
                         <ul className="menu d-flex align-items-center gap-5">
                             {
                                 nav__links.map((item, index) => (
@@ -96,7 +99,7 @@ const Header = () => {
                             {/* <Button className='btn secondary__btn'><Link to='/login'>Login</Link></Button>
                             <Button className='btn primary__btn'><Link to='/register'>Register</Link></Button> */}
                         </div>
-                        <span className="mobile__menu">
+                        <span className="mobile__menu" onClick={toggleMenu}>
                             <i className='ri-menu-line'></i>
                         </span>
                     </div>
--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useContext } from 'react';
 import '../styles/tour-details.css';
 import { Container, Row, Col, Form, ListGroup } from 'reactstrap';
 import { useParams } from 'react-router-dom';
@@ -10,11 +10,14 @@ import Newsletter from './../shared/Newsletter';
 import useFetch from './../hooks/useFetch';
 import { BASE_URL } from './../utils/config';
 
+import { AuthContext } from './../context/AuthContext'
+
 const TourDetails = () => {
 
     const { id } = useParams();
     const reviewMsgRef = useRef('');
     const [tourRating, setTourRating] = useState(null);
+    const { user } = useContext(AuthContext)
 
     // fetch data from database
     const { data: tour, loading, error } = useFetch(`${BASE_URL}/tours/${id}`)
@@ -28,9 +31,43 @@ const TourDetails = () => {
 
 
     // submit request to the server
-    const submitHandler = e => {
+    const submitHandler = async e => {
         e.preventDefault()
         const reviewText = reviewMsgRef.current.value;
+
+
+
+        try {
+
+            if (!user || user === undefined || user === null) {
+                alert('Please sign in')
+            }
+
+            const reviewObj = {
+                username: user?.username,
+                reviewText,
+                rating: tourRating
+            }
+
+            const res = await fetch(`${BASE_URL}/review/${id}`, {
+                method: 'post',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                credentials: 'include',
+                body: JSON.stringify(reviewObj)
+            })
+
+            const result = await res.json()
+            if (!res.ok) {
+                return alert(result.message)
+            }
+
+            alert(result.message);
+        } catch (err) {
+            alert(err.message);
+        }
+
         // later will call out api
     };
 
@@ -119,16 +156,16 @@ const TourDetails = () => {
                                                     <div className="w-100">
                                                         <div className="d-flex align-items-center justify-content-between">
                                                             <div>
-                                                                <h5>muhib</h5>
-                                                                <p>{new Date("01-18-2023").toLocaleDateString(
+                                                                <h5>{review.username}</h5>
+                                                                <p>{new Date(review.createdAt).toLocaleDateString(
                                                                     "en-US", options
                                                                 )}</p>
                                                             </div>
                                                             <span className='d-flex align-items-center'>
-                                                                5<i className='ri-star-s-fill'></i>
+                                                                {review.rating}<i className='ri-star-s-fill'></i>
                                                             </span>
                                                         </div>
-                                                        <h6>Amazing tour</h6>
+                                                        <h6>{review.reviewText}</h6>
                                                     </div>
                                                 </div>
                                             ))
